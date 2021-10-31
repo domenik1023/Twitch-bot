b218f0d5e7e8cfd080f8250b0f5ae3690e67d5e5
domenik1023 <73834431+domenik1023@users.noreply.github.com>
2021-10-31 14:35:02 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+const tmi = require('tmi.js');
+
+const client = new tmi.Client({
+  options: { debug: true, messagesLogLevel: "debug" },
+  connection: {
+    secure: true,
+    reconnect: true
+  },
+  identity: {
+    username: 'domenik1024',
+    password: `oauth:${process.env.TWITCH_OAUTH_TOKEN}`
+  },
+  channels: [ 'teamspeak_tv', 'domenik1023' ]
+});
+
+client.connect();
+
+client.on('message', (channel, tags, message, self) => {
+  if (self) return;
+  if (tags['display-name'] == 'teamspeak_tv')
+    {
+      client.say(channel, '!redeem');
+      console.log(`[${channel}]: ${tags['display-name']}: ${message}`);
+      console.log(`[${channel}]: domenik1024: !redeem || Send!`)
+    }
+    else if(tags['display-name'] == 'viantes')
+    {
+      client.say(channel, '!redeem');
+      console.log(`[${channel}]: ${tags['display-name']}: ${message}`);
+      console.log(`[${channel}]: domenik1024: !redeem || Send!`)
+    }
+    else if(tags['display-name'] == 'soraundshiro')
+    {
+      client.say(channel, '!redeem');
+      console.log(`[${channel}]: ${tags['display-name']}: ${message}`);
+      console.log(`[${channel}]: domenik1024: !redeem || Send!`)
+    }
+    else if(tags['display-name'] == 'domenik1023')
+    {
+      client.say(channel, '!redeem');
+      console.log(`[${channel}]: ${tags['display-name']}: ${message}`);
+      console.log(`[${channel}]: domenik1024: !redeem || Send!`)
+    }
+    else
+    {
+    }
+});
